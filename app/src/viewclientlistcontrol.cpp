2b17c815adaf2226841faae4df25f30376367a69
Farhoud <farhoud.talebi@carleton.ca>
2019-04-08 18:58:11 -0400

    fixed client login

--- app/src/viewclientlistcontrol.cpp
+++ app/src/viewclientlistcontrol.cpp
@@ -7,10 +7,11 @@ ViewClientListControl::ViewClientListControl()
 
 void ViewClientListControl::launchLogin(Ui::MainWindow *ui, ClientListModel *client_model)
 {
+    ui->clientLoginClientListView->setModel(client_model);
     ui->mainStackedWidget->setCurrentWidget(ui->clientLoginPage);
 }
 
 void ViewClientListControl::launch(Ui::MainWindow *ui, ClientListModel *client_model)
-{
+{    
     ui->mainStackedWidget->setCurrentWidget(ui->clientsPage);
 }
